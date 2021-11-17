4df766831e2b9dbaad6baad624d8e14600d2803f
Rohan Patel <patel@rohans-air.lan>
2021-11-17 17:48:09 -0600

    vuex skeleton up

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
+import { createStore } from 'vuex'
 import education from '@/store/modules/education'
 import projects from '@/store/modules/projects'
 import skills from '@/store/modules/skills'
 import socialMedia from '@/store/modules/socialMedia'
 import work from '@/store/modules/work'
 
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-    modules: {
-        education,
-        projects,
-        skills,
-        socialMedia,
-        work
-    }
-});
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    education,
+    projects,
+    skills,
+    socialMedia,
+    work
+  }
+})
