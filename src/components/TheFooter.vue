98a90853645cc32a236d9f8791a3f42639daf128
Rohan Patel <patel@rohans-air.lan>
2021-11-17 23:15:50 -0600

    new build

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,29 +1,27 @@
 <template>
   <v-divider></v-divider>
-  <v-card height="100%">
-    <v-footer>
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="text-center"
-      >
-        <v-card-text>
-          <v-btn
-            v-for="v in values"
-            :key="v.icon"
-            class="mx-4"
-            icon
-            size="x-large"
-          >
-            <v-icon size="24px">
-              {{ v.icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-  </v-card>
+  <v-footer>
+    <v-card
+      flat
+      tile
+      width="100%"
+      class="text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="v in values"
+          :key="v.icon"
+          class="mx-4"
+          icon
+          size="x-large"
+        >
+          <v-icon size="24px">
+            {{ v.icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+    </v-card>
+  </v-footer>
 </template>
 
 <script>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,26 +1,21 @@
 <template>
     <header>
-      <v-parallax 
-        dark
-        src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+      <v-row
+        align="center"
+        justify="center"
       >
-        <v-row
-          align="center"
-          justify="center"
+        <v-col
+          class="text-center"
+          cols="12"
         >
-          <v-col
-            class="text-center"
-            cols="12"
-          >
-            <h1 class="text-h4 font-weight-thin mb-4">
-              Rohan Patel
-            </h1>
-            <h4 class="subheading">
-              Build your application today!
-            </h4>
-          </v-col>
-        </v-row>
-      </v-parallax>
+          <h1 class="text-h4 font-weight-thin mb-4">
+            Rohan Patel
+          </h1>
+          <h4 class="subheading">
+            Build your application today!
+          </h4>
+        </v-col>
+      </v-row>
     </header>
 </template>
 
